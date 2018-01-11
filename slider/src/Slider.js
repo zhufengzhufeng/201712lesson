939dac2c81510b76960f84dc3fd8ec6a6a9e97f8
zhufengzhufeng <894918097@qq.com>
2018-01-11 17:25:38 +0800

    'slider'

--- slider/src/Slider.js
+++ slider/src/Slider.js
@@ -9,8 +9,15 @@ export default class Slider extends React.Component {
   }
   go =(step)=>{ // 去哪 传入要动几个
     let index = this.state.index+step; //先加的
-    if(index === this.props.items.length){ //当等于最后一张时 越界回到0
-      index = 0;
+    if(index > this.props.items.length){ //当等于最后一张时 越界回到0
+      this.$ul.style.transitionDuration = ''; //清除ul上的动画
+      this.$ul.style.left = 0; //回到0处
+      setTimeout(()=>{ //等动画移除后并且回到了0点 再增加回动画时间
+        this.$ul.style.transitionDuration = this.props.speed+'s';//再增加回来这个动画
+        index = 1; //下一次该走1了
+        this.setState({index});
+      },30);
+      return;//因为设置了setTimeout所以要等待setTimeout后再设置最新状态
     }
     if(index<0){ // 当小于第一张时 回到最后一张
       index = this.props.items.length - 1;
@@ -28,6 +35,7 @@ export default class Slider extends React.Component {
     if(this.props.autoplay){
         this.turn();
     }
+    this.$ul = this.refs.list.refs.ul;//获取sliderList中的ul元素
   }
   render(){
     console.log(1)
@@ -35,7 +43,7 @@ export default class Slider extends React.Component {
       <div className="slider-container" onMouseEnter={()=>{
         clearInterval(this.timer);
       }} onMouseLeave={()=>{this.turn();}}>
-        <SliderList index={this.state.index} items={this.props.items} speed={this.props.speed}/>
+        <SliderList ref="list" index={this.state.index} items={this.props.items} speed={this.props.speed}/>
         {this.props.arrows?<SliderArrows go={this.go}/>:null}
         {this.props.dots?<SliderDots go={this.go} items={this.props.items} index={this.state.index}/>:null}
       </div>
--- slider/src/SliderList.js
+++ slider/src/SliderList.js
@@ -2,15 +2,17 @@ import React from 'react';
 export default class SliderList extends React.Component {
   render(){
     let style = {
-      width:this.props.items.length*400+'px', //设置ul默认宽度
+      width:(this.props.items.length+1)*400+'px', //设置ul默认宽度
       left:this.props.index*400*-1 + 'px', //根据当前index 移动left值
       transition:`left ${this.props.speed}s linear`
     };
     return (
-      <ul style={style}>
+      <ul style={style} ref="ul">
         {this.props.items.map((item,index)=>(
           <li key={index}><img src={item.src} /></li>
         ))}
+        {/*实现无缝轮播要再增加一张图*/}
+        <li><img src={this.props.items[0].src}/></li>
       </ul>
     )
   }
