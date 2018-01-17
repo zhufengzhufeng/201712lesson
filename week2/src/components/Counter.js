99274e6b6b328a515a9d1c2bec20a7e6eda989e8
zhufengzhufeng <894918097@qq.com>
2018-01-17 12:24:23 +0800

    bindActionCreators

--- week2/src/components/Counter.js
+++ week2/src/components/Counter.js
@@ -19,7 +19,7 @@ class Counter extends React.Component {
         //store.dispatch(actions.add(10));
         this.props.add(1)
       }}>+</button>
-      <p>{this.props.n1}</p>
+      <p>{this.props.num}</p>
       <button onClick={()=>{
         //store.dispatch(actions.minus(10));
         this.props.minus(2)
@@ -32,7 +32,7 @@ class Counter extends React.Component {
 // 2.mapDispatchToProps 将dispatch方法映射成属性
 // 3.这个两个组件的返回值会作为当前组件的属性
 let mapStateToProps = (state) =>{ //这个函数的参数是state   state= store.getState()
-  return {n1:state.counter.num}
+  return {n1:state.counter.num} // {...state.counter} = > {num:0}
 };
 let mapDispatchToProps = (dispatch) =>{ // 这个函数的参数是dispatch dispatch = store.dispatch
   return {
@@ -44,4 +44,17 @@ let mapDispatchToProps = (dispatch) =>{ // 这个函数的参数是dispatch disp
     }
   }
 };
-export default connect(mapStateToProps,mapDispatchToProps)(Counter);//第二次执行的参数是当前组件
+// 这个方法是redux中的
+let bindActionCreators = (actions) =>{ //为什么可以直接传入一个actions,在内部会用这个函数进行包装
+  return (dispatch)=>{
+    let obj = {};
+    for(let key in actions){
+      obj[key] = (...args)=>{
+        dispatch(actions[key](...args))
+      }
+    }
+    return obj;
+  }
+};
+// connect中的mapDispatchToProps可以传入一个actionCreator对象
+export default connect(state=>({...state.counter}),bindActionCreators(actions))(Counter);//第二次执行的参数是当前组件
